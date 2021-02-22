3230532f8a64c199c997bf9b8c8b6002df5c8d35
keenan-burnett <keenburn2004@gmail.com>
2021-02-22 15:36:45 -0500

    More debugging of saving cartesians. (cv::imwrite() only saves files with 8-bit integer channels, CV_32F channels not supported.

--- src/save_cartesians.cpp
+++ src/save_cartesians.cpp
@@ -19,6 +19,7 @@ int main(int argc, const char *argv[]) {
     std::string cart_folder = radar_folder + "cart/";
     std::string mask_folder = radar_folder + "mask/";
     omp_set_num_threads(8);
+    cv::setNumThreads(8);
     get_file_names(radar_folder, radar_files, "png");
     for (uint idx = 0; idx < radar_files.size(); ++idx) {
         std::cout << idx << " / " << radar_files.size() - 1 << std::endl;
@@ -29,11 +30,10 @@ int main(int argc, const char *argv[]) {
         load_radar(radar_folder + radar_files[idx], timestamps, azimuths, valid, polar, navtech_version);
         cv::Mat cart;
         radar_polar_to_cartesian(azimuths, polar, radar_resolution, cart_resolution, cart_pixel_width,
-            interpolate_crossover, cart, navtech_version);
-        cv::imwrite(cart_folder + radar_files[idx], cart);
+            interpolate_crossover, cart, CV_32F, navtech_version);
         // mask
         cv::Mat polar_mask = cv::Mat::zeros(polar.rows, polar.cols, CV_32F);
-	#pragma omp parallel for
+        #pragma omp parallel for
         for (int i = 0; i < polar.rows; ++i) {
             float mean = 0;
             for (int j = 0; j < polar.cols; ++j) {
@@ -44,10 +44,15 @@ int main(int argc, const char *argv[]) {
                 polar_mask.at<float>(i, j) = polar.at<float>(i, j) > (mean * 3.0);
             }
         }
-	cv::Mat mask;
+        cv::Mat mask;
         radar_polar_to_cartesian(azimuths, polar_mask, radar_resolution, cart_resolution, cart_pixel_width,
-            interpolate_crossover, mask, navtech_version);
-        cv::imwrite(mask_folder + radar_files[idx], mask);
+            interpolate_crossover, mask, CV_32F, navtech_version);
+
+        cv::Mat cart_out, mask_out;
+        cart.convertTo(cart_out, CV_8UC1, 255.0);
+        mask.convertTo(mask_out, CV_8UC1, 255.0);
+        cv::imwrite(cart_folder + radar_files[idx], cart_out);
+        cv::imwrite(mask_folder + radar_files[idx], mask_out);
     }
     return 0;
 }
